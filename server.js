68b42684ec0ef7c481c155fd75fc594a9c30b62f
sgautier-dev <sgautier.dev@gmail.com>
2023-02-06 14:49:02 +0400

    cors set

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json()); //parse incoming data as JSON 
 
 app.use(cors({
-    origin: ['http://localhost:4200', 'https://your-server-domain.com'],
+    origin: ['http://localhost:4200', 'https://www.sgautier.dev/'],
     methods: ['POST'],
     allowedHeaders: ['Content-Type']
 }));
